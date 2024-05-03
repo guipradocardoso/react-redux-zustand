b4ba384c431fd262b5346ec9a1254304462e3a13
Guilherme Prado <91210198+guipradocardoso@users.noreply.github.com>
2024-05-02 22:27:31 -0300

    Migrating from Redux to Zustand

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,8 +1,8 @@
 import * as Collapsible from '@radix-ui/react-collapsible';
 import { ChevronDown } from "lucide-react";
+
 import { Lesson } from "./Lesson";
-import { useAppDispatch, useAppSelector } from '../store';
-import { play } from '../store/slices/player';
+import { useStore } from '../zustand-store';
 
 interface ModuleProps{
    moduleIndex: number
@@ -11,16 +11,13 @@ interface ModuleProps{
 }
 
 export function Module({ moduleIndex, title, amoutOfLessons }: ModuleProps) {
-   const dispatch = useAppDispatch()
-
-   const { currentModuleIndex, currentLessonIndex} = useAppSelector(state => {
-      const { currentModuleIndex, currentLessonIndex } = state.player
-
-      return { currentModuleIndex, currentLessonIndex}
-   })
-
-   const lessons = useAppSelector((state) => {
-      return state.player.course?.modules[moduleIndex].lessons
+   const {currentModuleIndex, currentLessonIndex, play, lessons } = useStore(store => {
+      return {
+         lessons: store.course?.modules[moduleIndex].lessons,
+         currentModuleIndex: store.currentModuleIndex,
+         currentLessonIndex: store.currentLessonIndex,
+         play: store.play
+      }
    })
 
    return (
@@ -53,7 +50,7 @@ export function Module({ moduleIndex, title, amoutOfLessons }: ModuleProps) {
                         key={lesson.id} 
                         title={lesson.title} 
                         duration={lesson.duration}
-                        onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
+                        onPlay={() => play([moduleIndex, lessonIndex])}
                         isCurrent={isCurrent}
                      />
                   )
--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,21 +1,23 @@
 import { MessageCircle } from 'lucide-react'
+import { useCurrentLesson, useStore } from '../zustand-store'
 
 import { Header } from '../components/Header'
 import { Video } from '../components/Video'
 import { Module } from '../components/Module'
-import { useAppDispatch, useAppSelector } from '../store'
-import { loadCourse, useCurrentLesson } from '../store/slices/player'
 import { useEffect } from 'react'
 
 export function Player() {
-   const dispatch = useAppDispatch()
+   const { course, load } = useStore(store => {
+      return {
+         course: store.course,
+         load: store.load,
+      }
+   })
 
-   const modules = useAppSelector(state => state.player.course?.modules)
-   
    const { currentLesson } = useCurrentLesson()
 
    useEffect(() => {
-      dispatch(loadCourse())
+      load()
    }, [])
 
    useEffect(() => {
@@ -43,7 +45,7 @@ export function Player() {
 
                <aside className='w-80 absolute top-0 bottom-0 right-0 border-l border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800 divide-y-2 divide-zinc-900'>
 
-                  { modules && modules.map((module, index) => {
+                  { course?.modules && course?.modules.map((module, index) => {
                      return (
                         <Module
                            key={module.id}
